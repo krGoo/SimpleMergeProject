8b6c231833686ad78738463e3e4a1a61e2da49a6
KrWoon <bcp0109@gmail.com>
2017-05-27 03:44:45 +0900

    Delete First.java

--- First.java
+++
@@ -1,8 +0,0 @@
-
-public class First {
-	public static void main(String[] args) {
-		System.out.println("Hello JAVA World");
-		System.out.println("Hello GitHub World");
-		System.out.println("New?");
-	}
-}
