f7abffad705b46f1951d45f6c180b313598185aa
rlaskarb09 <rlaskarb09@naver.com>
2017-06-06 13:35:58 +0900

    src

---
+++ src/Controller.java
@@ -0,0 +1,281 @@
+
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JTextArea;
+import javax.swing.event.*;
+import javax.swing.event.CaretListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Highlighter;
+import javax.swing.text.Utilities;
+
+
+public class Controller {
+	
+	
+    int linenum = 1;
+   
+   
+   public  void loadFile(Model dataSet, File ldfile, int num){
+      dataSet.load(ldfile, num);
+   }
+   
+   public  void saveFile(Model dataSet, File svfile, String content, int num) throws IOException{
+      dataSet.save(svfile, content, num);
+   }
+   
+   public  void saveStringToModel(Model dataSet, int num){
+      dataSet.saveString(num);
+   }   
+   
+   public  void copyToLeft(Model dataSet, viewview left, viewview right) {      	      
+	  findBox(dataSet, 0, linenum);
+	  compareText(dataSet, left,right);
+   } 
+   
+   public  void copyToRight(Model dataSet, viewview left, viewview right) {
+	  findBox(dataSet, 1, linenum);
+      compareText(dataSet, left,right);
+   }
+   
+   public void AllcopyToLeft(Model dataSet, viewview left, viewview right) {
+	      for(int i=0; i<dataSet.getSize("right"); i++) {
+	         dataSet.setString("left", i, dataSet.getString("right",i));
+	         dataSet.setBoolean("left", i, dataSet.getBoolean("right", i));
+	      }
+	      compareText(dataSet, left,right);
+	   }
+	   
+	   public void AllcopyToRight(Model dataSet, viewview left, viewview right) {
+	      for(int i=0; i<dataSet.getSize("left"); i++) {
+	         dataSet.setString("right", i, dataSet.getString("left",i));
+	         dataSet.setBoolean("right", i, dataSet.getBoolean("left", i));
+	      }
+	      compareText(dataSet, left,right);
+	   }
+   
+   
+   public  void compareText(Model dataSet, viewview left, viewview right) {
+	   // Clean Boolean
+	  dataSet.left_Boolean.clear();
+	  dataSet.right_Boolean.clear(); 
+      
+	  LCS_algorithm(dataSet);
+	  
+      // Lining with same string               
+      for(int k=0; k<dataSet.getSize("right") && k<dataSet.getSize("left"); k++) {
+         if(dataSet.getBoolean("left", k).equals(false) && dataSet.getBoolean("right", k).equals(true)) {
+            dataSet.addString("right", k, "");
+            dataSet.addBoolean("right", k, false);
+         }
+         else if(dataSet.getBoolean("left", k).equals(true) && dataSet.getBoolean("right", k).equals(false) ) {
+            dataSet.addString("left", k, "");
+            dataSet.addBoolean("left", k, false);
+         }            
+      }      
+            
+      // set Same line
+      setSameLining(dataSet);      
+
+   // if both lines are empty, their booleans are true
+      for(int k=0; k<dataSet.getSize("right"); k++) {
+         if(dataSet.getString("left", k).equals("") && dataSet.getBoolean("left", k).equals(false))
+            dataSet.setBoolean("left", k, true);
+         else if(dataSet.getString("left", k).equals("") && dataSet.getBoolean("left", k).equals(true))
+             dataSet.setBoolean("left", k, false);
+         
+         if(dataSet.getString("right", k).equals("") && dataSet.getBoolean("right", k).equals(false))
+             dataSet.setBoolean("right", k, true);   
+         else if(dataSet.getString("right", k).equals("") && dataSet.getBoolean("right", k).equals(true))
+             dataSet.setBoolean("right", k, false);           
+      }
+      
+      for(int k=0; k<dataSet.getSize("right"); k++) {
+         if(!dataSet.getBoolean("left", k).equals(dataSet.getBoolean("right", k))) {
+            dataSet.setBoolean("left", k, false);
+            dataSet.setBoolean("right", k, false);
+         }              
+      }
+      
+      // set Text new
+      remakeText(dataSet, left, right);            
+      
+      // Highlighting false line
+      Highlighting(dataSet, left, right);       
+   }   
+   
+   
+   
+   public  void setLineNum(int line){
+	   linenum=line;
+   }
+   public  void incereaselineNum(){
+       linenum++;
+   }
+   
+   
+   
+   public  void setSameLining(Model dataSet) {
+      while(dataSet.getSize("left") > dataSet.getSize("right")) {
+    	  dataSet.addString("right","");
+    	  dataSet.addBoolean("right",false);
+      }
+      while(dataSet.getSize("left") < dataSet.getSize("right")) {
+    	  dataSet.addString("left","");
+    	  dataSet.addBoolean("left",false);
+      }
+   }
+   
+   public  void findBox(Model dataSet, int num, int line){
+	      int up = line - 1;
+	      int down = line - 1;
+	      
+	      while(dataSet.getBoolean("right", up).equals(false) && up>0)	      						// find false up line  
+	          up--;
+	      while(dataSet.getBoolean("left", down).equals(false) && down< dataSet.getSize("left")-1)	// find false down line         
+	          down++;
+	      
+	      if(num == 0) {
+	    	  for(int i=up+1; i<= down+1; i++)
+	    		  dataSet.setString("left", i-1, dataSet.getString("right", i-1));
+	      }
+	      else {
+	    	  for(int i=up+1; i<= down+1; i++)
+	    		  dataSet.setString("right", i-1, dataSet.getString("left", i-1));
+	      }
+	   }
+   
+   public  void remakeText(Model dataSet, viewview left, viewview right) {
+	// set new textPane
+	      String x = "";
+	      String y = "";
+	      
+	      for(int k=0; k<dataSet.getSize("left"); k++) 
+	 	     x += dataSet.getString("left", k) +  "\n";
+	     
+	      
+	      for(int k=0; k<dataSet.getSize("right"); k++)
+	 	      y += dataSet.getString("right", k) +  "\n";
+	     
+	      left.textPane.setText(x);
+	      right.textPane.setText(y);
+   }
+   
+   public  void Highlighting(Model dataSet, viewview left, viewview right) {
+	// initialize Highlighter
+	      DefaultHighlighter Lhighlighter =  (DefaultHighlighter)left.textPane.getHighlighter();
+	      DefaultHighlighter Rhighlighter =  (DefaultHighlighter)right.textPane.getHighlighter();
+	      
+	      Highlighter.HighlightPainter paint = new DefaultHighlighter.DefaultHighlightPainter(Color.GREEN);
+	   
+	// highlighting
+	      int r=0;
+	      int l=0;
+	      
+	      Lhighlighter.setDrawsLayeredHighlights(false);
+	      Rhighlighter.setDrawsLayeredHighlights(false);
+	       
+	      try {         
+	         int n=0;
+	         int m=0;
+	         
+	         for(l=0; l<dataSet.getSize("left"); l++) {
+	            int leftStart=left.textPane.getLineStartOffset(n);
+	            int leftEnd = left.textPane.getLineEndOffset(n);
+	            
+	            if(dataSet.getBoolean("left", l).equals(false)) {
+	               Lhighlighter.addHighlight(leftStart, leftEnd, paint);
+	            }            
+	               leftStart += dataSet.getString("left", l).length() + 1;
+	               leftEnd = leftStart +  dataSet.getString("left", l).length();
+	               n++;
+	               
+	          }
+	         
+	         for(r=0; r<dataSet.getSize("right"); r++) {
+	            
+	            int rightStart= right.textPane.getLineStartOffset(m);
+	            int rightEnd  = right.textPane.getLineEndOffset(m);
+	            
+	            if(dataSet.getBoolean("right", r).equals(false)){
+	               Rhighlighter.addHighlight(rightStart, rightEnd, paint);
+	            }
+	               rightStart += dataSet.getString("right", r).length() + 1;   
+	               rightEnd = rightStart +  dataSet.getString("right", r).length();
+	               m++;
+	         }         
+	   
+	      } catch (BadLocationException e) {
+	         e.printStackTrace();
+	      }
+   }
+   
+   
+   public  void LCS_algorithm(Model dataSet) {
+	   int rightSize = dataSet.getSize("right") + 1;
+	      int leftSize = dataSet.getSize("left") + 1;
+	      int lcs[][] = new int[leftSize][rightSize];      
+	      
+	      //initialize Boolean
+	      for(int i=0; i<rightSize; i++) 
+	    	  dataSet.addBoolean("right", false);  
+	      for(int i=0; i<leftSize; i++)
+	    	  dataSet.addBoolean("left", false);  
+
+	      // add "0" for easy LCS algorithm
+	      dataSet.addString("right", 0, "0"); 
+	      dataSet.addString("left", 0, "0");     
+	      
+	      // LCS algorithm with ignoring empty line
+	      for(int i=1; i<leftSize; i++) {
+	         
+	         for(int j=1; j<rightSize; j++) {
+	            
+	            if(i == 0 || j == 0) {
+	               lcs[i][j] = 0;
+	               continue;
+	            }
+	            
+	            if((!dataSet.getString("left",i).equals("") || !dataSet.getString("right",j).equals("")) 
+	            		&& dataSet.getString("left",i).equals(dataSet.getString("right",j))) {
+	               lcs[i][j] = lcs[i-1][j-1] + 1;
+	            }
+	            else {
+	               if(lcs[i-1][j] > lcs[i][j-1])
+	                  lcs[i][j] = lcs[i-1][j];
+	               else
+	                  lcs[i][j] = lcs[i][j-1];
+	            }   
+	         }
+	      }      
+	      
+	      // backtracking for LCS
+	      int i = leftSize - 1;
+	      int j = rightSize - 1;      
+	      while(lcs[i][j] != 0) {
+	         
+	         if(lcs[i][j] == lcs[i][j-1]) {
+	            j--;
+	         }
+	         else if(lcs[i][j] == lcs[i-1][j]) {
+	            i--;
+	         }
+	         else if(lcs[i][j] - 1 == lcs[i-1][j-1]) {
+	        	dataSet.setBoolean("left", i, true);
+	        	dataSet.setBoolean("right", j, true);         
+	            i--;
+	            j--;
+	         }
+	      }      
+	      
+	      // remove "0"
+	      dataSet.right_String.remove(0);   dataSet.right_Boolean.remove(0);
+	      dataSet.left_String.remove(0);   dataSet.left_Boolean.remove(0);
+   }
+   
+}
+   
---
+++ src/MergeTest.java
@@ -0,0 +1,189 @@
+package view;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import junit.extensions.abbot.*;
+import abbot.tester.*;
+
+import java.awt.Button;
+import java.awt.event.*;
+
+
+public class MergeTest extends TestCase{
+	//ComponentTester guiTester = new ComponentTester();
+	protected void setUp(){
+		Model.file = new File[2];
+		Model.left_String = new ArrayList<String>();
+		Model.right_String = new ArrayList<String>();
+		Model.left_Boolean = new ArrayList<Boolean>();
+		Model.right_Boolean = new ArrayList<Boolean>();
+	}
+	protected void tearDown(){
+		
+	}
+	public void testFileTypeFilter(){
+		FileTypeFilter testUnit = new FileTypeFilter(".txt","Text File");
+		assertEquals(testUnit.getDescription(), "Text File (*.txt)");
+		File fileTest;
+		fileTest = new File("D:\\1.txt");
+		assertTrue(testUnit.accept(fileTest));
+		fileTest = new File("");
+		assertFalse(testUnit.accept(fileTest));
+		
+	}
+	public void testModel(){
+		
+		File fileTest_case1;
+		File fileTest_case2;
+		File saveFileTest;
+		fileTest_case1 = new File("D:\\1.txt");
+		fileTest_case2 = new File("D:\\2.txt");
+		saveFileTest = new File("D:\\3");
+		
+		Model.load(fileTest_case1, 0);
+		Model.load(fileTest_case2, 1);
+		assertEquals(Model.getfile(0).getName(),"1.txt");
+		assertEquals(Model.getfile(1).getName(),"2.txt");
+		
+		
+		Model.saveString(0);
+		Model.saveString(1);
+		
+		for(int i = 1; i <= 7; i++){
+			assertEquals(Model.left_String.get(i-1), Integer.toString(i));
+		}
+		int sample[] = {2,1,3,8,4,9,5,7,6,8};
+		for(int i = 0; i < sample.length; i++){
+			assertEquals(Model.right_String.get(i), Integer.toString(sample[i]));
+		}
+		
+		assertEquals(Model.getString(0), "1\n2\n3\n4\n5\n6\n7\n");
+		assertEquals(Model.getString(1), "2\n1\n3\n8\n4\n9\n5\n7\n6\n8\n");
+		
+		
+		try {
+			Model.save(saveFileTest, "abc", 0);
+			File testFile = new File("D:\\3.txt");
+			assertTrue(testFile.exists());
+			BufferedReader br = new BufferedReader(new FileReader(testFile));
+			String testString = br.readLine();
+			assertEquals(testString, "abc");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Controller.LCS_algorithm();
+		assertFalse(Model.getBoolean("left", 0));
+		assertTrue(Model.getBoolean("right", 0));
+		assertEquals(Model.getString("left",0), "1");
+		assertEquals(Model.getString("right",0), "2");
+		Model.setBoolean("left", 0, true);
+		Model.setBoolean("right", 0, false);
+		assertTrue(Model.getBoolean("left", 0));
+		assertFalse(Model.getBoolean("right", 0));
+		Model.setString("left", 0, "2");
+		Model.setString("right", 0, "1");
+		assertEquals(Model.getString("left",0), "2");
+		assertEquals(Model.getString("right",0), "1");
+		
+	}
+	public void testController(){
+		File fileTest_case1;
+		File fileTest_case2;
+		fileTest_case1 = new File("D:\\1.txt");
+		fileTest_case2 = new File("D:\\2.txt");
+		Model.load(fileTest_case1, 0);
+		Model.load(fileTest_case2, 1);
+		
+		Controller.LCS_algorithm();
+		int[] leftSet = {0,1,1,1,1,0,1};
+		int[] rightSet = {1,0,1,0,1,0,1,1,0,0};
+		for(int i = 0; i < Model.left_Boolean.size(); i++){
+			if(leftSet[i] == 0)
+				assertFalse(Model.left_Boolean.get(i));
+			else
+				assertTrue(Model.left_Boolean.get(i));
+		}
+		for(int i = 0; i < Model.right_Boolean.size(); i++){
+			if(rightSet[i] == 0)
+				assertFalse(Model.right_Boolean.get(i));
+			else
+				assertTrue(Model.right_Boolean.get(i));
+		}
+		
+		leftview leftTestView = new leftview();
+		rightview rightTestView = new rightview();
+		Controller.remakeText(leftTestView, rightTestView);
+		
+		assertEquals("1\n2\n3\n4\n5\n6\n7\n",leftTestView.textPane.getText());
+		assertEquals("2\n1\n3\n8\n4\n9\n5\n7\n6\n8\n",rightTestView.textPane.getText());
+		
+		Controller.setSameLining();
+		
+		assertEquals(Model.getSize("left"), Model.getSize("right"));
+		
+		assertEquals(1, Controller.linenum);
+		Controller.setLineNum(4);
+		assertEquals(4, Controller.linenum);
+		Controller.incereaselineNum();
+		assertEquals(5, Controller.linenum);
+		Controller.setLineNum(1);
+		assertEquals(1, Controller.linenum);
+		
+		
+	}
+	public void testleftview(){
+		leftview testview = new leftview();
+		assertEquals(testview.loadbtn.getText(), "Load");
+		assertEquals(testview.editbtn.getText(), "Edit");
+		assertEquals(testview.savebtn.getText(), "Save");
+		assertTrue(testview.scrollPane.isEnabled());
+		assertTrue(testview.textPane.isEnabled());
+		//ButtonTest guiTest = new ButtonTest("loadbtn");
+		//guiTest.testButton();
+		
+	}
+	public void testrightview(){
+		rightview testview = new rightview();
+		assertEquals(testview.loadbtn.getText(), "Load");
+		assertEquals(testview.editbtn.getText(), "Edit");
+		assertEquals(testview.savebtn.getText(), "Save");
+		assertTrue(testview.scrollPane.isEnabled());
+		assertTrue(testview.textPane.isEnabled());
+	}
+	/*
+	public class ButtonTest extends ComponentTestFixture{
+		private String clickType;
+		public ButtonTest (String name) {super(name);}
+		private ComponentTester tester;
+	    protected void setUp() {
+	        tester = new ComponentTester();
+	    }
+	    protected void tearDown() {
+
+	        tester = null;
+	    }
+	    public void testClick() {
+	        ActionListener al = new ActionListener() {
+	            public void actionPerformed(ActionEvent ev) {
+	                clickType = ev.getActionCommand();                            
+	            }
+	        };
+	    }
+	    public void testButton(){
+	    	setUp();
+	    	leftview testview = new leftview();
+	    	//tester.click(testview.loadbtn);
+	    	assertEquals(clickType, Button.ABORT);
+	    	tearDown();
+	    }
+	}
+	*/
+	
+}
