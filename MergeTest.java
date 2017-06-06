7e7b64a23d2858dbced9765abe5abcd76532cd87
krGoo <fotokhg@naver.com>
2017-06-06 10:42:34 +0900

    06.06

--- MergeTest.java
+++ MergeTest.java
@@ -1,11 +1,31 @@
 package view;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.InputEvent;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
+import junit.extensions.abbot.*;
+import abbot.tester.*;
+
+import java.awt.Button;
+import java.awt.event.*;
+
 
 public class MergeTest extends TestCase{
+	//ComponentTester guiTester = new ComponentTester();
+	protected void setUp(){
+		Model.file = new File[2];
+		Model.left_String = new ArrayList<String>();
+		Model.right_String = new ArrayList<String>();
+		Model.left_Boolean = new ArrayList<Boolean>();
+		Model.right_Boolean = new ArrayList<Boolean>();
+	}
+	protected void tearDown(){
+		
+	}
 	public void testFileTypeFilter(){
 		FileTypeFilter testUnit = new FileTypeFilter(".txt","Text File");
 		assertEquals(testUnit.getDescription(), "Text File (*.txt)");
@@ -20,8 +40,10 @@ public class MergeTest extends TestCase{
 		
 		File fileTest_case1;
 		File fileTest_case2;
+		File saveFileTest;
 		fileTest_case1 = new File("D:\\1.txt");
 		fileTest_case2 = new File("D:\\2.txt");
+		saveFileTest = new File("D:\\3");
 		
 		Model.load(fileTest_case1, 0);
 		Model.load(fileTest_case2, 1);
@@ -43,8 +65,42 @@ public class MergeTest extends TestCase{
 		assertEquals(Model.getString(0), "1\n2\n3\n4\n5\n6\n7\n");
 		assertEquals(Model.getString(1), "2\n1\n3\n8\n4\n9\n5\n7\n6\n8\n");
 		
+		
+		try {
+			Model.save(saveFileTest, "abc", 0);
+			File testFile = new File("D:\\3.txt");
+			assertTrue(testFile.exists());
+			BufferedReader br = new BufferedReader(new FileReader(testFile));
+			String testString = br.readLine();
+			assertEquals(testString, "abc");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Controller.LCS_algorithm();
+		assertFalse(Model.getBoolean("left", 0));
+		assertTrue(Model.getBoolean("right", 0));
+		assertEquals(Model.getString("left",0), "1");
+		assertEquals(Model.getString("right",0), "2");
+		Model.setBoolean("left", 0, true);
+		Model.setBoolean("right", 0, false);
+		assertTrue(Model.getBoolean("left", 0));
+		assertFalse(Model.getBoolean("right", 0));
+		Model.setString("left", 0, "2");
+		Model.setString("right", 0, "1");
+		assertEquals(Model.getString("left",0), "2");
+		assertEquals(Model.getString("right",0), "1");
+		
 	}
 	public void testController(){
+		File fileTest_case1;
+		File fileTest_case2;
+		fileTest_case1 = new File("D:\\1.txt");
+		fileTest_case2 = new File("D:\\2.txt");
+		Model.load(fileTest_case1, 0);
+		Model.load(fileTest_case2, 1);
+		
 		Controller.LCS_algorithm();
 		int[] leftSet = {0,1,1,1,1,0,1};
 		int[] rightSet = {1,0,1,0,1,0,1,1,0,0};
@@ -65,20 +121,20 @@ public class MergeTest extends TestCase{
 		rightview rightTestView = new rightview();
 		Controller.remakeText(leftTestView, rightTestView);
 		
-		assertEquals(leftTestView.textPane.getText(), "1\n2\n3\n4\n5\n6\n7\n");
-		assertEquals(rightTestView.textPane.getText(), "2\n1\n3\n8\n4\n9\n5\n7\n6\n8\n");
+		assertEquals("1\n2\n3\n4\n5\n6\n7\n",leftTestView.textPane.getText());
+		assertEquals("2\n1\n3\n8\n4\n9\n5\n7\n6\n8\n",rightTestView.textPane.getText());
 		
 		Controller.setSameLining();
 		
 		assertEquals(Model.getSize("left"), Model.getSize("right"));
 		
-		assertEquals(Controller.linenum, 1);
+		assertEquals(1, Controller.linenum);
 		Controller.setLineNum(4);
-		assertEquals(Controller.linenum, 4);
+		assertEquals(4, Controller.linenum);
 		Controller.incereaselineNum();
-		assertEquals(Controller.linenum, 5);
+		assertEquals(5, Controller.linenum);
 		Controller.setLineNum(1);
-		assertEquals(Controller.linenum, 1);
+		assertEquals(1, Controller.linenum);
 		
 		
 	}
@@ -89,8 +145,8 @@ public class MergeTest extends TestCase{
 		assertEquals(testview.savebtn.getText(), "Save");
 		assertTrue(testview.scrollPane.isEnabled());
 		assertTrue(testview.textPane.isEnabled());
-		//testview.loadbtn.doClick();
-		//testview.loadbtn.getActionListeners()[0].actionPerformed(null);
+		//ButtonTest guiTest = new ButtonTest("loadbtn");
+		//guiTest.testButton();
 		
 	}
 	public void testrightview(){
@@ -101,5 +157,33 @@ public class MergeTest extends TestCase{
 		assertTrue(testview.scrollPane.isEnabled());
 		assertTrue(testview.textPane.isEnabled());
 	}
+	/*
+	public class ButtonTest extends ComponentTestFixture{
+		private String clickType;
+		public ButtonTest (String name) {super(name);}
+		private ComponentTester tester;
+	    protected void setUp() {
+	        tester = new ComponentTester();
+	    }
+	    protected void tearDown() {
+
+	        tester = null;
+	    }
+	    public void testClick() {
+	        ActionListener al = new ActionListener() {
+	            public void actionPerformed(ActionEvent ev) {
+	                clickType = ev.getActionCommand();                            
+	            }
+	        };
+	    }
+	    public void testButton(){
+	    	setUp();
+	    	leftview testview = new leftview();
+	    	//tester.click(testview.loadbtn);
+	    	assertEquals(clickType, Button.ABORT);
+	    	tearDown();
+	    }
+	}
+	*/
 	
 }
